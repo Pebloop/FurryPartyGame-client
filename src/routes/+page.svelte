40511c34bc49b935ae2a5b03e4fe56f4aac5d8be
Pebloop <pebloop@outlook.com>
2024-12-08 05:49:17 +0100

    listen to websocket

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,22 @@
 
     onMount(() => {
         socket = new WebSocket('wss://ws-fpg.pebloop.dev');
+
+        socket.onopen = function (event) {
+            console.log('Connected to server');
+        };
+
+        socket.onmessage = function (event) {
+            console.log('Message from server ', event.data);
+        };
+
+        socket.onclose = function (event) {
+            console.log('Disconnected from server');
+        };
+
+        socket.addEventListener('error', function (event) {
+            console.log('Error: ', event);
+        });
     });
 
     function join() {
@@ -21,9 +37,9 @@
     }
 </script>
 
-<div class="flex flex-col">
-<h1>Furry Party Game</h1>
-<input type="text" id="name" placeholder="Enter your name" value={name} on:change={e => name = e.target.value}/>
-<input type="text" id="room" placeholder="Enter room code" value={room} on:change={e => room = e.target.value}/>
-<button id="join" on:click={join}>Join</button>
+<div class="flex flex-col" id="join-form">
+    <h1>Furry Party Game</h1>
+    <input type="text" id="name" placeholder="Enter your name" value={name} on:change={e => name = e.target.value}/>
+    <input type="text" id="room" placeholder="Enter room code" value={room} on:change={e => room = e.target.value}/>
+    <button id="join" on:click={join}>Join</button>
 </div>
