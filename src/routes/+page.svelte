6fc1951485af384ea86ba21c68e7512b60750ae1
Pebloop <pebloop@outlook.com>
2024-12-08 03:11:56 +0100

    Set col

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+    let name = '';
+    let room = '';
 
+    let socket = new WebSocket('ws://146.59.153.243:8001');
+
+    function join() {
+        if (name && room) {
+            socket.send(JSON.stringify({
+                type: 'join',
+                name,
+                room
+            }));
+        }
+    }
 </script>
 
 <div class="flex flex-col">
 <h1>Furry Party Game</h1>
-<input type="text" id="name" placeholder="Enter your name"/>
-<input type="text" id="room" placeholder="Enter room code"/>
-<button id="join">Join</button>
+<input type="text" id="name" placeholder="Enter your name" value={name} on:change={e => name = e.target.value}/>
+<input type="text" id="room" placeholder="Enter room code" value={room} on:change={e => room = e.target.value}/>
+<button id="join" on:click={join}>Join</button>
 </div>
