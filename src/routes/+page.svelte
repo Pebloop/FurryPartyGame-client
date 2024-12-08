55860a399e52970ab8ffd97ecea763535bc4d543
Pebloop <pebloop@outlook.com>
2024-12-08 06:09:04 +0100

    Decorate join form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,12 @@
 
         socket.addEventListener('room_joined', function (event) {
             console.log('Room joined: ', event);
+
+            // delete join form
+            document.getElementById('join-form').remove();
+
+            // show game
+
         });
     });
 
@@ -41,9 +47,13 @@
     }
 </script>
 
-<div class="flex flex-col" id="join-form">
-    <h1>Furry Party Game</h1>
-    <input type="text" id="name" placeholder="Enter your name" value={name} on:change={e => name = e.target.value}/>
-    <input type="text" id="room" placeholder="Enter room code" value={room} on:change={e => room = e.target.value}/>
-    <button id="join" on:click={join}>Join</button>
+<div class="flex flex-col items-center justify-center h-screen" id="join-form">
+    <div class="flex flex-col w-80 bg-gray-500 rounded-2xl items-center">
+        <img src="/logo.png" alt="logo" class="w-32"/>
+        <div class="flex flex-col p-4 w-full items-center">
+            <input type="text" id="name" placeholder="Enter your name" value={name} on:change={e => name = e.target.value} class="mb-2 rounded-2xl h-8 w-full pl-2"/>
+            <input type="text" id="room" placeholder="Enter room code" value={room} on:change={e => room = e.target.value} class="mb-2 rounded-2xl h-8 w-full pl-2"/>
+            <button id="join" on:click={join} class="bg-blue-500 text-white rounded-2xl h-8 w-1/3 mt-3">Join</button>
+        </div>
+    </div>
 </div>
