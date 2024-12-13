58eaa26b174f762c8521efded6f2f05aac3e6b5c
Pebloop <pebloop@outlook.com>
2024-12-13 19:10:50 +0100

    Update RunRunRun pad

--- src/lib/games/runrunrun/RunRunRunGame.svelte
+++ src/lib/games/runrunrun/RunRunRunGame.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 
+    import {onMount} from "svelte";
+
     export let player: String;
     export let code: String;
     export let socket: WebSocket;
 
     let isCrouching = false;
+
+    onMount(() => {
+        // jump if space is pressed
+        window.addEventListener('keydown', (e) => {
+            if (e.code === 'Space') {
+                jump();
+            }
+            if (e.code === 'ShiftLeft') {
+                crouch();
+            }
+        });
+    });
 
     function jump() {
         console.log('Jumping');
@@ -47,10 +61,31 @@
 
 </script>
 
-<div>
-    <h1>Run Run Run</h1>
-    <h1>Player: {player}</h1>
-    <h1>Code: {code}</h1>
-    <button id="jump" on:click={jump} class="w-2/3 h-10" >Jump</button>
-    <button id="crouch" on:click={crouch} class="w-2/3 h-10">Crouch</button>
+<div class="flex flex-col items-center w-full h-screen">
+    <div class="flex flex-row justify-between w-full p-2 bg-gray-600 text-white h-10">
+        <h1>Run Run Run !</h1>
+        <h1>{player}</h1>
+        <h1>{code}</h1>
+    </div>
+    <div class="flex flex-row justify-between w-full p-2 items-center full_height buttons">
+        <button id="jump" on:click={jump} class="w-1/2 m-3 bg-gray-500 rounded-2xl text-white h-full button">Jump</button>
+        <button id="crouch" on:click={crouch} class="w-1/2 bg-gray-500 rounded-2xl text-white h-full button">Crouch</button>
+    </div>
 </div>
+
+<style>
+    .full_height {
+        height: calc(100% - 40px);
+    }
+
+    @media (max-width: 600px) {
+        .buttons {
+            flex-direction: column;
+        }
+
+        .button {
+            height: 50%;
+            width: 100%;
+        }
+    }
+</style>
